cd3d52336d00573301e8380fa399385b6c10d60e
Andrew Trac <andrew@gigeco.net>
2022-10-15 14:17:58 -0400

    registration page

--- src/Components/RegistrationPage.js
+++ src/Components/RegistrationPage.js
@@ -1,8 +1,34 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./RegistrationPage.css";
 
+const defaultObject = {
+  user: "test2@email.com",
+  question_1: true,
+  question_2: false,
+  question_3: false,
+  question_4: true,
+  question_5: true,
+  question_6: false,
+  question_7: true,
+  question_8: false,
+  question_9: false,
+  question_10: true,
+  question_11: false,
+  question_12: false,
+  question_13: true,
+  question_14: false,
+  question_15: false,
+  question_16: false,
+  question_17: true,
+  question_18: false,
+  question_19: false,
+  question_20: false,
+  question_21: true,
+};
+
 const RegistrationPage = (props) => {
-  const [formSubmission, setFormSubmission] = useState({});
+  const [formSubmission, setFormSubmission] = useState(defaultObject);
 
   const handleClick = (event) => {
     const newState = { ...formSubmission };
@@ -12,10 +38,41 @@ const RegistrationPage = (props) => {
     setFormSubmission(newState);
   };
 
+  const handleSubmit = () => {
+    axios.defaults.headers.post["Content-Type"] =
+      "application/x-www-form-urlencoded";
+
+    axios
+      .post(`https://terribledatingapp-api.herokuapp.com/`, formSubmission)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
+
   const questionArray = [
-    "Do you want children?",
-    "Are you a smoker?",
-    "Do you drink?",
+    ["Do you want children?", "Yes", "No"],
+    ["Are you a smoker?", "Yes", "No"],
+    ["Do you drink?", "Yes", "No"],
+    ["Do you eat meat?", "Yes", "No"],
+    ["Are you a smoker?", "Yes", "No"],
+    ["Are you religious?", "Yes", "No"],
+    ["Do you like spicy food?", "Yes", "No"],
+    ["Do you prefer coffee or tea?", "Yes", "No"],
+    ["Do you prefer cats or dogs?", "Cats", "Dogs"],
+    ["Are you tall or short?", "Tall", "Short"],
+    ["Do you use Android or iPhone?", "Android", "iPhone"],
+    ["Backstreet Boys or Nsync?", "BSB", "NSYNC"],
+    ["Do you like pina coladas", "Yes", "No"],
+    ["And getting caught in the rain?", "Yes", "No"],
+    ["What’s your side of the bed?", "Left", "Right"],
+    ["Are you a night owl or a early bird?", "Owl", "Bird"],
+    ["Is it to-may-toe or to-mah-toe?", "To-may-toe", "To-mah-toe"],
+    ["What’s your level of commitment?", "Long", "Short"],
+    ["Do you prefer cake or pie?", "Cake", "Pie"],
+    ["Top or bottom?", "Top", "Bottom"],
+    ["Ruffled chips or smooth?", "Ruffle", "Smooth"],
+    ["Do you like to party? ;)", "Yes", "No"]
   ];
 
   return (
@@ -27,19 +84,30 @@ const RegistrationPage = (props) => {
         {questionArray.map((question, index) => {
           return (
             <div className="registrationpage__item">
-              <div className="registrationpage__question">{question}</div>
-              <button id={index} value={true} onClick={handleClick}>
-                yes
+              <div className="registrationpage__question">{question[0]}</div>
+              <button
+                id={index+1}
+                className="button_selection"
+                value={true}
+                onClick={handleClick}
+              >
+                {question[1]}
               </button>
-              <button id={index} value={false} onClick={handleClick}>
-                no
+              <button
+                id={index+1}
+                className="button_selection"
+                value={false}
+                style={{ marginLeft: "5px" }}
+                onClick={handleClick}
+              >
+                {question[2]}
               </button>
             </div>
           );
         })}
       </div>
       <div className="registrationpage__footer">
-        <button>Submit</button>
+        <button className="button_selection" onClick={handleSubmit}>Submit</button>
       </div>
     </div>
   );
