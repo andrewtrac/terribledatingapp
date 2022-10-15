a3447ff0b997b3306e82c7c65172ae653a465a42
Andrew Trac <andrew@gigeco.net>
2022-10-15 15:26:27 -0400

    added profile page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegistrationPage from "./Components/RegistrationPage.js";
 import BioPage from "./Components/BioPage.js";
+import ProfilePage from "./Components/ProfilePage/ProfilePage.js";
 import "./App.css";
 import FrontPage from "./Components/FrontPage.js";
 import { useState } from "react";
