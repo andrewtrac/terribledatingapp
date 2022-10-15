b8aff184ac7f64b7401b80574c9caaf17800a6d3
Andrew Trac <andrew@gigeco.net>
2022-10-15 13:15:07 -0400

    new updates

--- src/Components/RegistrationPage.js
+++ src/Components/RegistrationPage.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RegistrationPage.css";
 
 const RegistrationPage = (props) => {
+  const [formSubmission, setFormSubmission] = useState({});
+
+  const handleClick = (event) => {
+    const newState = { ...formSubmission };
+    const selector = `question_${event.currentTarget.id}`;
+    newState[selector] = event.currentTarget.value;
+    console.log(newState);
+    setFormSubmission(newState);
+  };
+
   const questionArray = [
     "Do you want children?",
     "Are you a smoker?",
@@ -14,9 +24,22 @@ const RegistrationPage = (props) => {
         Lets get to know each other!
       </div>
       <div className="registrationpage__form">
-        {questionArray.map((question) => {
-          return <div className="registrationpage__item">{question}</div>;
+        {questionArray.map((question, index) => {
+          return (
+            <div className="registrationpage__item">
+              <div className="registrationpage__question">{question}</div>
+              <button id={index} value={true} onClick={handleClick}>
+                yes
+              </button>
+              <button id={index} value={false} onClick={handleClick}>
+                no
+              </button>
+            </div>
+          );
         })}
+      </div>
+      <div className="registrationpage__footer">
+        <button>Submit</button>
       </div>
     </div>
   );
