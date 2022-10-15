ddf5034cc0edc2fc567c935e2c170e6122a2e17a
Andrew Trac <andrew@gigeco.net>
2022-10-15 12:22:48 -0400

    registration page update

--- src/Components/RegistrationPage.js
+++ src/Components/RegistrationPage.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./RegistrationPage.css";
 
 const RegistrationPage = (props) => {
+  const questionArray = [
+    "Do you want children?",
+    "Are you a smoker?",
+    "Do you drink?",
+  ];
 
   return (
-    <div>
-      TEST123
+    <div className="registrationpage__body">
+      <div className="registrationpage__header">
+        Lets get to know each other!
+      </div>
+      <div className="registrationpage__form">
+        {questionArray.map((question) => {
+          return <div className="registrationpage__item">{question}</div>;
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RegistrationPage
+export default RegistrationPage;
